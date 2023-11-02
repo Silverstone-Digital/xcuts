1c64da7e6b2fa7349b8b226984317b14cdc39ee4
sumanmazumdertier5 <suman.mazumder@tier5.in>
2023-11-02 23:29:56 +0530

    first change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
-  // content: ["*"],
+  // content: ["./src/*.{html,js}"],
+  content: ["*"],
   theme: {
     screens: {
       'sm': '576px',
